790aa776d67e6ecc4691cac2d872ca07561249ef
Robert-George Crupa <robertcrupa@outlook.com>
2026-04-02 11:18:24 +0200

    Solved crystal ball

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const h = breaks.length;
+  const jump = Math.floor( Math.sqrt(h) );
+  let curr = 0;
 
-}
+  while (curr < h && breaks[curr] === false) {
+    curr += jump;
+  }
+
+  if( curr > h ) return -1;
+
+  while (breaks[curr] === true) {
+    curr -= 1; 
+  }
+
+  return curr + 1;
+} 
