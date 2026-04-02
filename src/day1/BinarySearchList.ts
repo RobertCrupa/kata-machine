6de2e735a7070dbfca6bf99b42a6c673abd919d9
Robert-George Crupa <robertcrupa@outlook.com>
2026-04-02 11:08:00 +0200

    Solved binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let min = 0;
+  let max = haystack.length;
+
+  while(min < max) {
+    let mid = Math.floor( min + (max - min) / 2);
+    let num = haystack[mid];
+
+    if( num === needle ) {
+      return true;
+    } else if (num < needle) {
+      min = mid + 1;
+    } else {
+      max = mid;
+    }
+  }
+
+  return false;
 
 }
